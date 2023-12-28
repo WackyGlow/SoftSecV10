3d3d7a76a6d6da6992f01bbbb97e7ec6f2c37ec7
Rasmus Thyregod Kristensen <70566908+WackyGlow@users.noreply.github.com>
2023-12-29 00:22:21 +0100

    as

--- ChatClient/DiffieHellman.cs
+++ ChatClient/DiffieHellman.cs
@@ -1,6 +1,20 @@
-﻿namespace ChatClient;
+﻿using System.Security.Cryptography;
+
+namespace ChatClient;
 
 public class DiffieHellman
 {
-    
+    private ECDiffieHellmanCng dh = new()
+    {
+        KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash,
+        HashAlgorithm = CngAlgorithm.Sha256
+    };
+
+    public byte[] PublicKey => dh.PublicKey.ToByteArray();
+
+    public byte[] GenerateKey(byte[] otherPublicKey)
+    {
+        using var otherPubKey = ECDiffieHellmanCngPublicKey.FromByteArray(otherPublicKey, CngKeyBlobFormat.EccPublicBlob);
+        return dh.DeriveKeyMaterial(otherPubKey);
+    }
 }
